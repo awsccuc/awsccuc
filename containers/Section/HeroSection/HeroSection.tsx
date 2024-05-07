39949dd09f0b72b0a2a4495c09de77a9de8e727e
awsccuc <awsccuc.pnc@gmail.com>
2024-05-08 07:19:07 +0800

    fix: added responsiveness fixes

--- containers/Section/HeroSection/HeroSection.tsx
+++ containers/Section/HeroSection/HeroSection.tsx
@@ -1,12 +1,12 @@
+import accounts from '@/sources/accounts';
 import Link from 'next/link';
 import { RiSparkling2Fill } from 'react-icons/ri';
-import accounts from '@/sources/accounts';
 
 function HeroSection() {
 	return (
 		<section className='min-h-screen w-screen bg-nature bg-center bg-no-repeat bg-cover'>
-			<div className='flex flex-col items-center justify-center p-8 sm:p-12 md:p-16 lg:p-24 outline min-h-screen gap-8'>
-				<div className='flex items-center gap-2 backdrop-blur-sm bg-white/90 rounded-xl py-2 px-8 drop-shadow-md'>
+			<div className='flex flex-col items-center justify-center sm:p-8 md:p-16 lg:p-24 min-h-screen gap-8'>
+				<div className='flex items-center gap-2 backdrop-blur-sm bg-white/90 rounded-xl py-3 sm:py-2 px-8 drop-shadow-md'>
 					<span>
 						<RiSparkling2Fill />
 					</span>
@@ -14,7 +14,7 @@ function HeroSection() {
 						href={
 							'https://www.facebook.com/awsccpnc/posts/pfbid06JLpassZuGMngWR8R6GdTkHRzyJGdtaNxDoWASwgKTs7bv2EpnnwuHs3gRHeWjZal'
 						}
-						className='text-xl font-bold tracking-tight'
+						className='text-lg md:text-xl font-bold tracking-tight text-center leading-none'
 					>
 						Skill Builders out now
 					</Link>
@@ -23,12 +23,12 @@ function HeroSection() {
 					</span>
 				</div>
 				<div>
-					<h1 className='text-5xl md:text-7xl lg:text-8xl font-extrabold text-white tracking-tight uppercase text-center text-balance drop-shadow-xl'>
+					<h1 className='text-3xl sm:text-6xl md:text-7xl lg:text-8xl font-extrabold text-white tracking-tight uppercase text-center text-balance drop-shadow-xl'>
 						A passionate club in the vast jungle of opportunities within the AWS
 						ecosystem
 					</h1>
 				</div>
-				<div className='flex gap-8 md:gap-10 py-2 px-8'>
+				<div className='flex gap-6 sm:gap-8 md:gap-10 py-2 px-8'>
 					{accounts.map((account) => {
 						return (
 							<Link
--- layouts/Nav/Nav.tsx
+++ layouts/Nav/Nav.tsx
@@ -1,27 +1,42 @@
+import { FaHome, FaNewspaper, FaSignal } from 'react-icons/fa';
+
 import Link from 'next/link';
+import { FaUserGroup } from 'react-icons/fa6';
 
 function Nav() {
 	return (
 		<nav className='fixed pt-6 w-full z-10'>
-			<div className='flex justify-evenly items-center rounded-xl w-[85%] md:w-[75%] mx-auto p-4 backdrop-blur-sm bg-white/90 shadow-sm'>
+			<div className='flex justify-evenly items-center rounded-xl w-[92%] md:w-[75%] mx-auto py-4 px-2 backdrop-blur-sm bg-white/90 shadow-sm'>
 				<Link href='/'>
-					<span className='text-center font-extrabold text-xl sm:text-2xl hover:text-[#01E58B]'>
-						home
+					<span className='text-center font-extrabold text-2xl hover:text-[#01E58B]'>
+						<span className='hidden sm:block'>home</span>
+						<span className='block sm:hidden'>
+							<FaHome />
+						</span>
 					</span>
 				</Link>
 				<Link href='#about'>
-					<span className='text-center font-extrabold text-xl sm:text-2xl hover:text-[#01E58B]'>
-						about
+					<span className='text-center font-extrabold text-2xl hover:text-[#01E58B]'>
+						<span className='hidden sm:block'>about</span>
+						<span className='block sm:hidden'>
+							<FaUserGroup />
+						</span>
 					</span>
 				</Link>
 				<Link href='/'>
-					<span className='text-center font-extrabold text-xl sm:text-2xl hover:text-[#01E58B]'>
-						news
+					<span className='text-center font-extrabold text-2xl hover:text-[#01E58B]'>
+						<span className='hidden sm:block'>news</span>
+						<span className='block sm:hidden'>
+							<FaNewspaper />
+						</span>
 					</span>
 				</Link>
 				<Link href='/'>
-					<span className='text-center font-extrabold text-xl sm:text-2xl hover:text-[#01E58B]'>
-						connect
+					<span className='text-center font-extrabold text-2xl hover:text-[#01E58B]'>
+						<span className='hidden sm:block'>connect</span>
+						<span className='block sm:hidden'>
+							<FaSignal />
+						</span>
 					</span>
 				</Link>
 			</div>
